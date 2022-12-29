9f1dace74857b6a531d9319dfa3233cab28707f6
Nfassory Diaby <fassory.diaby@gmail.com>
2022-12-29 22:39:19 +0100

    add product api

--- server/product/product_repository.go
+++ server/product/product_repository.go
@@ -69,17 +69,22 @@ func (r *repository) Update(id int, inputProduct InputProduct) (Product, error) 
 	product.Name = inputProduct.Name
 	product.Price = inputProduct.Price
 
-    uncite := r.db.Where(&Product{Name: product.Name}).First(&product).Error
-    if uncite != nil {
+    if product.Name != "" {
+        uncite := r.db.Where(&Product{Name: product.Name}).First(&product).Error
+        if uncite != nil {
+            err = r.db.Save(&product).Error
+            if err != nil {
+                return product, err
+            }
+        } else {
+            return product, errors.New("The Product name already exist")
+        }
+    }else{
         err = r.db.Save(&product).Error
         if err != nil {
             return product, err
         }
-    } else {
-        return product, errors.New("The Product name already exist")
     }
-
-
 
 	return product, nil
 }
